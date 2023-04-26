bab96fc4ed5c5f6bf6a4e93cdfb1d129f20fbedd
Manuel velasco <manuelvelasco2020@gmail.com>
2023-04-25 21:23:52 -0700

    adding if else conditional to the game

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-let firstCard = 9;
-let secondCard = 8;
+let firstCard = 12;
+let secondCard = 11;
 
 let sum = firstCard + secondCard;
 
-console.log(sum);
+if (sum < 21) {
+  console.log('do you want to draw a new card?');
+} else if (sum === 21) {
+  console.log("Whohoo!!! You've got blackjack! winner!");
+} else {
+  console.log("You're out of the game!");
+}
