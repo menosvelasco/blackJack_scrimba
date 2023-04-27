77f28b1843303b928ade0fc04efc30430e6ac2fb
Manuel velasco <manuelvelasco2020@gmail.com>
2023-04-26 21:30:08 -0700

    adding two variable on player status of the game

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 let firstCard = 12;
 let secondCard = 11;
-
 let sum = firstCard + secondCard;
+/* player status */
+let hasBlackJack = false;
+let isAlive = true;
 
 if (sum < 21) {
   console.log('do you want to draw a new card?');
 } else if (sum === 21) {
   console.log("Whohoo!!! You've got blackjack! winner!");
+  /* player has blackjack */
+  hasBlackJack = true;
 } else {
   console.log("You're out of the game!");
+  /* player is out of the game. */
+  isAlive = false;
 }
