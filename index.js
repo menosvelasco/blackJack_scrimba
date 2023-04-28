cd8f97ea68bf98289122f0e2e5e5bec399390ff8
Manuel velasco <manuelvelasco2020@gmail.com>
2023-04-27 21:11:30 -0700

    declare a varible empty string and reassign it inside if condition

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-let firstCard = 12;
+let firstCard = 10;
 let secondCard = 11;
 let sum = firstCard + secondCard;
 /* player status */
 let hasBlackJack = false;
 let isAlive = true;
+/* reassign message variable */
+let message = '';
 
 if (sum < 21) {
-  console.log('do you want to draw a new card?');
+  message = 'do you want to draw a new card?';
 } else if (sum === 21) {
-  console.log("Whohoo!!! You've got blackjack! winner!");
+  message = "Whohoo!!! You've got blackjack! winner!";
   /* player has blackjack */
   hasBlackJack = true;
 } else {
-  console.log("You're out of the game!");
+  message = "You're out of the game!";
   /* player is out of the game. */
   isAlive = false;
 }
+
+console.log(message);
